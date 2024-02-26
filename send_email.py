c73ca2fd3ca66516d0ce858985507f724d480d2b
anim_tp <g.k.anim.tp1999@gmail.com>
2024-02-26 21:08:46 +0600

    Image added to the email successfully

--- send_email.py
+++ send_email.py
@@ -4,31 +4,23 @@ from email.mime.image import MIMEImage
 from pathlib import Path
 from string import Template
 import smtplib
-
-
-template_path = "Template/template.html"
-image_path = "Template/Image/ZAYYANA.png"
-
-
-with open(template_path, "r") as template_file:
-    template_content = template_file.read()
-
-# Create a Template object
-template = Template(template_content)
-
+tempalte = Template(Path("Template/template.html").read_text())
 
 # ftpromi0226@gmail.com
 message = MIMEMultipart()
 message["from"] = "g.k.anim.tp1999@gmail.com"
 message["to"] = "golam.kibria.anim@g.bracu.ac.bd"
 message["subject"] = "This is a test email"
-body = template.substitute(
+body = tempalte.substitute(
     {"name": "Promi", "logo": "image/ZAYYANA.png"})
 message.attach(MIMEText(body, "html"))
-# message.attach(MIMEImage(Path("image/ZAYYANA.png").read_bytes()))
-with open(image_path, "rb") as image_file:
-    image = MIMEImage(image_file.read())
-    message.attach(image)
+
+
+image_reader = open("Template/Image/ZAYYANA.png", 'rb')
+image = MIMEImage(image_reader.read())
+image_reader.close()
+image.add_header('Content-ID', '<Mailtrapimage>')
+message.attach(image)
 
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
